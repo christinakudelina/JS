8fd1140b68580e44faff7861339bc6cbaf2c3698
Кристина Куделина <kristinakydelina@gmail.com>
2023-08-10 16:43:41 +0300

    objects JS

--- 06_objects/task_1.js
+++ 06_objects/task_1.js
@@ -1,49 +1,60 @@
 // Обязательная часть задания
-let user1={
+let user1 = {
   name: 'Игорь',
   age: 17
 }
 
-let user2={
+let user2 = {
   name: 'Оля',
   age: 21
 }
 
 function getOlderUser(userOne, userTwo){
-  // Здесь решение задачи
-}
+  if (user1.age > user2.age) {
+    return user1.name
+  } else {
+    return user2.name
+  }
+
+  }
 
 let result = getOlderUser(user1, user2)
 console.log('Старший пользователь:',result);
 
 
 // // Не обязательная часть задания
-// let allUsers=[
-//   {
-//     name: 'Валя',
-//     age: 11
-//   },
-//   {
-//     name: 'Таня',
-//     age: 24
-//   },
-//   {
-//     name: 'Рома',
-//     age: 21
-//   },
-//   {
-//     name: 'Надя',
-//     age: 34
-//   },
-//   {
-//     name: 'Антон',
-//     age: 7
-//   }
-// ]
+let allUsers = [
+  {
+    name: 'Валя',
+    age: 11
+  },
+  {
+    name: 'Таня',
+    age: 24
+  },
+  {
+    name: 'Рома',
+    age: 21
+  },
+  {
+    name: 'Надя',
+    age: 34
+  },
+  {
+    name: 'Антон',
+    age: 7
+  }
+]
 
-// function getOlderUserArray(usersArray){
-//   // Здесь решение задачи
-// }
+function getOlderUserArray(usersArray){
+  let arr = []
+  for (let i of usersArray) {
+    arr.push(i.age)
+  }
 
-// let result2 = getOlderUserArray(allUsers)
-// console.log('Старший пользователь:',result2);
+  let max = usersArray.filter( user => user.age >= Math.max(...arr))[0].name
+  return max
+}
+
+let result2 = getOlderUserArray(allUsers)
+console.log('Старший пользователь:',result2);
--- 06_objects/task_3.js
+++ 06_objects/task_3.js
@@ -2,14 +2,18 @@ let objects = [
   { name: 'Василий', surname: 'Васильев' },
   { name: 'Иван', surname: 'Иванов' },
   { name: 'Пётр', surname: 'Петров' }
-]
- 
+ ]
 
 function filter(arr, prop, value){
   // Здесь решение задачи
   // arr - массив объектов
   // prop - свойство по которому производится фильтрация
   // value - значение свойства по которому производится фильтрация
+  let newList = []
+  for (let item of arr) {
+    if ((item[prop]).includes(value) == true) newList.push(item)
+  }
+  return newList
 }
 
 let result = filter(objects, 'name', 'Иван');
