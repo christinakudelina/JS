6a2e1b08c7aef8478e5ee9dc78031af437c1452a
Кристина Куделина <kristinakydelina@gmail.com>
2023-08-15 13:43:54 +0300

    modification 3 and 4

--- 07_dom-basics-1/task_3.js
+++ 07_dom-basics-1/task_3.js
@@ -8,27 +8,26 @@ let allStudents = [
   { name: 'Антон', age: 7 }
 ]
 
+function createStudentCard(studentObj) {
+  let item = document.createElement('li');
+
+  let h2 = document.createElement('h2');
+  h2.textContent = studentObj.name
+  item.append(h2)
+
+  let span = document.createElement('span')
+  span.textContent = studentObj.age
+  item.append(span)
+
+  return item
+}
+
 function createStudentsList(listArr) {
   let list = document.createElement('ul')
 
   for (let student of listArr) {
-
-    function createStudentCard(studentObj) {
-      let item = document.createElement('li');
-
-      let h2 = document.createElement('h2');
-      h2.textContent = student.name
-      item.append(h2)
-
-      let span = document.createElement('span')
-      span.textContent = student.age
-      item.append(span)
-
-      return item
-    }
-    list.append(createStudentCard(allStudents))
+    list.append(createStudentCard(student))
   }
-
   return list
 }
 
